2bfd4f72ab02e1c46e8bfc304ce4e9bcfe35c242
Koichi Matsuda <k-matsuda@caldia.org>
2021-07-05 13:31:23 +0900

    他のパレットを使用する

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,11 @@
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// https://substrate.dev/docs/en/knowledgebase/runtime/frame
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
+use frame_support::{debug, decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
 use frame_system::ensure_signed;
+
+use pallet_timestamp as timestamp;
+use pallet_assets as assets;
 
 #[cfg(test)]
 mod mock;
@@ -14,9 +17,14 @@ mod mock;
 mod tests;
 
 /// Configure the pallet by specifying the parameters and types on which it depends.
-pub trait Config: frame_system::Config {
+pub trait Config: timestamp::Config + assets::Config + frame_system::Config {
 	/// Because this pallet emits events, it depends on the runtime's definition of an event.
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
+	type AssetsInterface: self::AssetsInterface<
+		Self::AssetId, 
+		<Self as frame_system::Config>::AccountId, 
+		Self::Balance>;
+
 }
 
 // The pallet's runtime storage items.
@@ -51,6 +59,25 @@ decl_error! {
 		StorageOverflow,
 	}
 }
+pub trait AssetsInterface<AssetId, AccountId, Balance>: pallet_assets::Config {
+	fn test(id: AssetId, who: AccountId) -> Balance ;
+}
+impl<T: Config> AssetsInterface<
+	<T as pallet_assets::Config>::AssetId,
+	<T as frame_system::Config>::AccountId,
+	<T as pallet_assets::Config>::Balance,
+	> 
+	for T 
+	// where
+	// T: pallet_assets::Config<AssetId = u32>,
+{
+	fn test(id: <T as pallet_assets::Config>::AssetId, who: <T as frame_system::Config>::AccountId) -> T::Balance 
+	{
+		// id: T::AssetId, who: impl sp_std::borrow::Borrow<T::AccountId>) -> T::Balance 
+		// <pallet_session::historical::Module<T>>::prune_up_to(up_to);
+		<pallet_assets::Module<T>>::balance(id, who)
+	}
+}
 
 // Dispatchable functions allows users to interact with the pallet and invoke state changes.
 // These functions materialize as "extrinsics", which are often compared to transactions.
@@ -62,21 +89,29 @@ decl_module! {
 
 		// Events must be initialized if they are used by the pallet.
 		fn deposit_event() = default;
-
+				
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
+		pub fn do_something(origin, something: <T as pallet_assets::Config>::AssetId) -> dispatch::DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
 			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
 			let who = ensure_signed(origin)?;
 
 			// Update storage.
-			Something::put(something);
+			// Something::put(something);
+			let _time = <timestamp::Module<T>>::get();
+			debug::info!("Timestamp: {:?}", _time);
+			// let id = 10; // as <T as pallet_assets::Config>::AssetId;
+			// let b = <assets::Module<T>>::balance(id, who);
+			debug::info!("Who: {:?}", &who);
+
+			let b = T::AssetsInterface::test(something, who);
+			debug::info!("Balance: {:?}", b);
 
 			// Emit an event.
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			// Self::deposit_event(RawEvent::SomethingStored(something, who));
 			// Return a successful DispatchResult
 			Ok(())
 		}
